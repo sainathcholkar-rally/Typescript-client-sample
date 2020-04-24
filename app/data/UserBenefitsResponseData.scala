28b987f9591f1c2c0a226c8f3eb73c352cbae937
Sainath Cholkar <sainath.cholkar@rallyhealth.com>
2020-04-24 10:03:41 +0530

    data

--- app/data/UserBenefitsResponseData.scala
+++ app/data/UserBenefitsResponseData.scala
@@ -6,37 +6,37 @@ import model.{BenefitModel, Demographics, UserBenefitsResponse}
 
 object UserBenefitsResponseData {
 
-  private val effectiveDate1 = new GregorianCalendar(2014, 2, 11)
-  private val terminationDate1 = new GregorianCalendar(2020, 2, 11)
-  private val lastUpdated1 = new GregorianCalendar(2020, 2, 12)
+  private val effectiveDate1 = new GregorianCalendar(2014, 2, 11, 11, 40, 40)
+  private val terminationDate1 = new GregorianCalendar(2020, 2, 11, 9, 47, 40)
+  private val lastUpdated1 = new GregorianCalendar(2020, 2, 12, 1, 12, 40)
 
-  private val effectiveDate2 = new GregorianCalendar(2016, 11, 1)
-  private val terminationDate2 = new GregorianCalendar(2019, 7, 21)
-  private val lastUpdated2 = new GregorianCalendar(2018, 3, 2)
+  private val effectiveDate2 = new GregorianCalendar(2016, 11, 1, 12, 34, 40)
+  private val terminationDate2 = new GregorianCalendar(2019, 7, 21, 8, 54, 40)
+  private val lastUpdated2 = new GregorianCalendar(2018, 3, 2, 7, 32, 40)
 
-  private val effectiveDate3 = new GregorianCalendar(2017, 2, 11)
-  private val terminationDate3 = new GregorianCalendar(2028, 2, 11)
+  private val effectiveDate3 = new GregorianCalendar(2017, 2, 11, 6, 9, 40)
+  private val terminationDate3 = new GregorianCalendar(2018, 2, 11, 11, 4, 40)
 
-  private val effectiveDate4 = new GregorianCalendar(1999, 2, 11)
-  private val terminationDate4 = new GregorianCalendar(2000, 7, 11)
+  private val effectiveDate4 = new GregorianCalendar(1999, 2, 11, 10, 54, 40)
+  private val terminationDate4 = new GregorianCalendar(2000, 7, 11, 5, 45, 40)
 
-  private val effectiveDate5 = new GregorianCalendar(2019, 4, 11)
-  private val terminationDate5 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate5 = new GregorianCalendar(2019, 4, 11, 3, 45, 40)
+  private val terminationDate5 = new GregorianCalendar(2020, 2, 11, 4, 45, 40)
 
-  private val effectiveDate6 = new GregorianCalendar(2013, 9, 14)
-  private val terminationDate6 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate6 = new GregorianCalendar(2013, 9, 14, 1, 45, 40)
+  private val terminationDate6 = new GregorianCalendar(2020, 2, 11, 2, 45, 40)
 
-  private val effectiveDate7 = new GregorianCalendar(2002, 2, 11)
-  private val terminationDate7 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate7 = new GregorianCalendar(2002, 2, 11, 10, 45, 40)
+  private val terminationDate7 = new GregorianCalendar(2020, 2, 11, 10, 45, 40)
 
-  private val effectiveDate8 = new GregorianCalendar(2003, 2, 11)
-  private val terminationDate8 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate8 = new GregorianCalendar(2003, 2, 11, 9, 45, 40)
+  private val terminationDate8 = new GregorianCalendar(2020, 2, 11, 8, 45, 40)
 
-  private val effectiveDate9 = new GregorianCalendar(2004, 2, 11)
-  private val terminationDate9 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate9 = new GregorianCalendar(2004, 2, 11, 6, 45, 40)
+  private val terminationDate9 = new GregorianCalendar(2020, 2, 11, 5, 45, 40)
 
-  private val effectiveDate10 = new GregorianCalendar(2020, 2, 11)
-  private val terminationDate10 = new GregorianCalendar(2020, 2, 11)
+  private val effectiveDate10 = new GregorianCalendar(2020, 2, 11, 4, 45, 40)
+  private val terminationDate10 = new GregorianCalendar(2020, 2, 11, 1, 45, 40)
 
   private val demographics = Demographics(firstName = "James", lastName = "Gosling", dob = "07/21/1960")
 
